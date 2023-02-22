8e93a5e28df528440fcfef0c6d74f42e5cda5c59
fx5344 <fox_files01@hotmail.com>
2023-02-22 16:06:56 +0100

    Yelp MVP

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :content, presence: true, allow_blank: false
+  validates :rating, presence: true, allow_blank: false
+  validates :rating, inclusion: { in: 0..5 }
+  validates :rating, numericality: { only_integer: true }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "french" }
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian"}
+antalia = {name: "Antalia", address: "3 bd lafaillette", category: "belgian" }
+doner = {name: "Doner", address: "5 rue felibien", category: "chinese" }
+king = {name: "King", address: "7 bd du duc", category: "french" }
+
+
+[dishoom, pizza_east, antalia, doner, king].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
